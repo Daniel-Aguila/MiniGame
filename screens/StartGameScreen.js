95e6be0c50ad8e9e64086fbaaf62657f1317d5f7
Daniel Aguila <danielbaguila@gmail.com>
2024-10-25 08:22:49 -0500

    added background and input functionality with bound rules

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import {StyleSheet, TextInput, View, Alert} from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen(){
+    const [enteredNumber, setEnteredNumber] = useState('');
+
+    function numberInputHandler(enteredText){
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler(){
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler(){
+        const chosenNumber = parseInt(enteredNumber);
+
+        //isNaN isNotANumber
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99 ) {
+            //first is title, then message, then configure buttons
+            Alert.alert('Invalid Number!',
+                'Number has to be a number between 1 and 99',
+            [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}])
+            return;
+        }
+        
+        console.log('Valid number!');
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType='number-pad'
+            autoCapitalize='none' autoCorrect='none' value={enteredNumber} onChangeText={numberInputHandler}/>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#4e0329',
+        borderRadius: 8,
+        //elevation is shadow Android
+        elevation: 4,
+        //shadow lets us add a shadow on IOS
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: 'center',
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold'
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
+    }
+})
